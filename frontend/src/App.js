2eca4424182d79dfe637fb3d770d1a5cdf669f51
Masud Rana <145442783+masud053@users.noreply.github.com>
2025-10-20 12:50:47 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Feed from './components/Feed';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
